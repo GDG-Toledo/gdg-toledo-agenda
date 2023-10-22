8edc47ae38eb4c59f74d092a9d916655420fcb90
Greg Miller <hello@gregmiller.io>
2023-10-22 13:10:14 -0400

    Update intro.tsx
    - Show name in intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,11 @@
 import { CMS_NAME } from '../lib/constants'
 
+
 const Intro = () => {
   return (
     <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
       <h1 className="text-5xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
-        Event Agenda
+        { CMS_NAME }
       </h1>
       <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
         Our agenda for Toledo Region tech events. 
