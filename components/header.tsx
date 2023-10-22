588044be3073ae6dcc28915965f3495d92dacf48
Greg Miller <hello@gregmiller.io>
2023-10-22 16:59:59 -0400

    Add logo to banner
    - Add SVG GDG Toledo
    - Place on intro and header

--- components/header.tsx
+++ components/header.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <h2 className="mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
-      <Link href="/" className="hover:underline">
+    <h2 className="inline-flex mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
+      <img width="50" src="/assets/images/gdg-toledo.svg" alt="GDG Toledo logo"></img>
+      <Link href="/" className="hover:underline ms-2">
         Event Agenda
       </Link>
       .
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,10 @@
 import { CMS_NAME } from '../lib/constants'
 
-
 const Intro = () => {
   return (
     <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
-      <h1 className="text-5xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
-        { CMS_NAME }
+      <h1 className="inline-flex gap-2 text-5xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
+        <img width="100" src="/assets/images/gdg-toledo.svg" alt="GDG Toledo logo"></img>{ CMS_NAME }
       </h1>
       <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
         Our agenda for Toledo Region tech events. 
