37e95cab61d8a4c3bf1be2269e315e53394cef6e
Greg Miller <hello@gregmiller.io>
2023-10-22 19:34:38 -0400

    Update intro.tsx
    - Align logo and heading for moble

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ import { CMS_NAME } from '../lib/constants'
 const Intro = () => {
   return (
     <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
-      <h1 className="inline-flex gap-2 text-5xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
+      <h1 className="inline-flex items-center gap-2 text-6xl font-bold leading-none tracking-tighter align-center sm:text-7xl md:text-6xl lg:text-7xl md:pr-8">
         <img width="100" src="/assets/images/gdg-toledo.svg" alt="GDG Toledo logo"></img>{ CMS_NAME }
       </h1>
       <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
