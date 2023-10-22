556a9c25014729f240ab180b790821f2b7504c3f
Greg Miller <hello@gregmiller.io>
2023-10-22 18:43:21 -0400

    Update cover-image.tsx
    - Evaluate if image exists
    - Set State to true if cover image exists
    - Return a placeholder image

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,36 +1,48 @@
-import cn from 'classnames'
-import Link from 'next/link'
-import Image from 'next/image'
+import cn from 'classnames';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
 
 type Props = {
-  title: string
-  src: string
-  slug?: string
+  title: string;
+  src: string;
+  slug?: string;
 }
 
 const CoverImage = ({ title, src, slug }: Props) => {
-  const image = (
+  const [imageExists, setImageExists] = useState(true);
+
+  const handleImageError = () => {
+    setImageExists(false);
+  }
+
+  const imageComponent = imageExists ? (
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
+      onError={handleImageError}
       className={cn('shadow-sm w-full', {
         'hover:shadow-lg transition-shadow duration-200': slug,
       })}
       width={1300}
       height={630}
     />
-  )
+  ) : (
+    
+    <Image src="/assets/blog/hello-world/cover.jpg" alt="Default Image" width={1300} height={630} />
+  );
+
   return (
     <div className="sm:mx-0">
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]" aria-label={title}>
-          {image}
+          {imageComponent}
         </Link>
       ) : (
-        image
+        imageComponent
       )}
     </div>
-  )
+  );
 }
 
-export default CoverImage
+export default CoverImage;
