3079c720b0c963d38e19ce82e1c0ff61c1223fbd
Greg Miller <hello@gregmiller.io>
2023-10-23 12:09:54 -0400

    Config setup
    - Final settings updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,30 +4,30 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier",
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'react'],
-  'jsx-runtime': {
-    plugins: ['react'],
+  plugins: ["@typescript-eslint", "react"],
+  "jsx-runtime": {
+    plugins: ["react"],
     parserOptions: {
       ecmaFeatures: {
         jsx: true,
@@ -35,8 +35,8 @@ module.exports = {
       jsxPragma: null, // for @typescript/eslint-parser
     },
     rules: {
-      'react/react-in-jsx-scope': 0,
-      'react/jsx-uses-react': 0,
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-uses-react": 0,
     },
   },
 };
